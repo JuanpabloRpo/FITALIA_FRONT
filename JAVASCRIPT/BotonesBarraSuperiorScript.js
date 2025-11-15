83a0b096ff71e942ab72d797932e8000246e05d3
wDroxl <davidmola2112@gmail.com>
2025-11-15 18:30:31 -0500

    Menu Hamburguesa
    Se agrego el menú hamburguesa en el gestionar perfil

--- JAVASCRIPT/BotonesBarraSuperiorScript.js
+++ JAVASCRIPT/BotonesBarraSuperiorScript.js
@@ -1,22 +1,5 @@
 document.addEventListener("DOMContentLoaded", () => { 
 
-  // ===== BOTONES DE LA BARRA SUPERIOR =====
-  document.getElementById("btnSaludFisica").addEventListener("click", () => {
-    window.location.href = "SaludFisica.html";
-  });  
-
-  document.getElementById("btnSaludMental").addEventListener("click", () => {
-    window.location.href = "SaludMental.html";
-  });
-
-  document.getElementById("btnNutricional").addEventListener("click", () => {
-    window.location.href = "Nutricional.html";
-  });
-
-  document.getElementById("btnHome").addEventListener("click", () => {
-    window.location.href = "PrincipalRegistrado.html";
-  });
-
 
   // ===== MENÚ HAMBURGUESA =====
   const menuIcon = document.getElementById("hamburgerMenu");
